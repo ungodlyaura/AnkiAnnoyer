45bb7ca365713fa73a128652c82416056a36b0bc
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-01-22 02:13:47 +0800

    update

--- AnkiAnnoyer.py
+++ AnkiAnnoyer.py
@@ -3,12 +3,13 @@ import PySimpleGUI as sg
 import keyboard
 import time
 import requests
+from glom import glom
 
 
-text_length_multiplier = 2 # 1 for english 2 for japanese
-text_length_multiplier2 = 1 # for showing the answer
-time_limit = 300 # seconds but /2 for some reason
+# Timing
+time_limit = 300 # seconds
 answer_cooldown = 300 # seconds
+# Keybinds
 close_app_keybind = 'ctrl+shift+.'
 show_answer_keybind = '0'
 rate_again_keybind = '1'
@@ -16,6 +17,14 @@ rate_bad_keybind = '2'
 rate_good_keybind = '3'
 rate_easy_keybind = '4'
 undo_answer_keybind = '5'
+# Text Type
+text_color = "white"
+text_length_multiplier = 2 # 1 for english 2 for japanese
+text_length_multiplier2 = 1 # for showing the answer
+# Pathway
+question_pathway = 'result.fields.keyword.value' # change this to the pathway of your deck's question and below one to the answer
+answer_pathway = 'result.fields.kanji.value'
+
 # TODO keybind to pause/unpause the script
 
 
@@ -77,26 +86,32 @@ class KeyWatcher:
             print("rate again")
             self.answer_showing = False
             rateCard(1)
+            getNewText()
         if keyboard.is_pressed(rate_bad_keybind):
             print("rate bad")
             self.answer_showing = False
             rateCard(2)
+            getNewText()
         if keyboard.is_pressed(rate_good_keybind):
             print("rate good")
             self.answer_showing = False
             rateCard(3)
+            getNewText()
         if keyboard.is_pressed(rate_easy_keybind):
             print("rate easy")
             self.answer_showing = False
             rateCard(4)
+            getNewText()
         if keyboard.is_pressed(undo_answer_keybind):
             print("undo answer")
             self.answer_showing = False
             undoAnswer()
+            getNewText()
         if keyboard.is_pressed(show_answer_keybind):
             print("show answer")
             self.answer_showing = True
             showAnswer()
+            getNewText()
 
     def start_watching(self):
         keyboard.hook(self.on_key_event)
@@ -112,24 +127,31 @@ key_watcher_thread = threading.Thread(target=key_watcher.start_watching)
 key_watcher_thread.start()
 
 
-current_text = getCurrentCard()["result"]["fields"]["keyword"]["value"]
+current_text = glom(getCurrentCard(),question_pathway)
+
+
+def getNewText():
+    global current_text
+    if key_watcher.answer_showing:
+        current_text = glom(getCurrentCard(),answer_pathway)
+    else:
+        current_text = glom(getCurrentCard(),question_pathway)
 
 
 # create a new window with updated text
 def updateWindow():
-
-    
-
+    global text_color
     print(current_text)
+    
     if key_watcher.answer_showing:
         font_size = int(min(1920 / (len(current_text)*text_length_multiplier2), 450))
         layout = [
             [
                 sg.Text(
-                    getCurrentCard()["result"]["fields"]["keyword"]["value"],
+                    glom(getCurrentCard(),question_pathway),
                     size=len(current_text)*text_length_multiplier2,
                     background_color="green",
-                    text_color="white",
+                    text_color=text_color,
                     expand_x=True,
                     expand_y=True,
                     font=("BIZ UDGothic", font_size,  "")
@@ -140,7 +162,7 @@ def updateWindow():
                     current_text,
                     size=len(current_text)*text_length_multiplier2,
                     background_color="green",
-                    text_color="white",
+                    text_color=text_color,
                     expand_x=True,
                     expand_y=True,
                     font=("BIZ UDGothic", font_size,  "")
@@ -155,7 +177,7 @@ def updateWindow():
                     current_text,
                     size=len(current_text)*text_length_multiplier,
                     background_color="green",
-                    text_color="white",
+                    text_color=text_color,
                     expand_x=True,
                     expand_y=True,
                     font=("BIZ UDGothic", font_size,  "")
@@ -175,38 +197,35 @@ def updateWindow():
 
 
 
-def windowThing(current_text,last_text):
-    if key_watcher.answer_showing:
-        last_text = current_text
+def windowThing():
+    global current_text
+    last_text = current_text
     window = updateWindow() # get new window
+    startTime = time.time()
+
     while current_text == last_text and key_watcher.running:
-        event, values = window.read(timeout=10)
+        window.read(timeout=10)
         now = time.time()
         fadeAmount = min((now - startTime) / time_limit, 1)
         window.set_alpha(fadeAmount)
-        if key_watcher.answer_showing:
-            current_text = getCurrentCard()["result"]["fields"]["kanji"]["value"]
-        else:
-            current_text = getCurrentCard()["result"]["fields"]["keyword"]["value"]
     window.close() #close the window
+
     return current_text
 
 
 
 #checks if ctrl + shift + . has been called
 while key_watcher.running:
-    last_text = current_text
-
     print("cooldown!")
     temp_time = answer_cooldown*10
     while temp_time > 0 and key_watcher.running:
         temp_time += -1
         time.sleep(0.1)
-    print("annoying time!")
-    startTime = time.time()
 
-    current_text = windowThing(current_text,last_text)
-    current_text = windowThing(current_text,last_text)
+    print("annoying time!")
+    current_text = windowThing()
+    if key_watcher.answer_showing:
+        current_text = windowThing()
 
 
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+# AnkiAnnoyer
+
+# Use:
+- Used to slowly display the kanji on your screen to annoy you to complete that card.
+- You have to use keybinds for everything to work properly
+- Grace period, time to be fully visible, keybinds, deck path, and kanji or english are configurable.
+
+# Instructions:
+1. Install AnkiConnect addon (2055492159)
+2. Set and save options at top of code
+3. Open a deck and start studying
+4. Run the code
