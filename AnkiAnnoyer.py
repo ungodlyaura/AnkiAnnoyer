600f9959048653f880c9d8a1707c57c405499c24
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-07-24 17:39:32 +0800

    Fixed smoothness and response time

--- AnkiAnnoyer.py
+++ AnkiAnnoyer.py
@@ -1,78 +1,99 @@
-import threading
-import PySimpleGUI as sg
-import keyboard
+import os
+import asyncio
 import time
-import requests
+
+try:
+    import aiohttp
+except ImportError:
+    print("Trying to install aiohttp")
+    os.system('python -m pip install aiohttp')
+import aiohttp
+
+try:
+    from glom import glom
+except ImportError:
+    print("Trying to install glom")
+    os.system('python -m pip install glom')
 from glom import glom
 
-############################################
+try:
+    from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
+except ImportError:
+    print("Trying to install PyQt5")
+    os.system('python -m pip install PyQt5')
+from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
+qWait = QtTest.QTest.qWait
+
+try:
+    import keyboard
+except ImportError:
+    print("Trying to install keyboard")
+    os.system('python -m pip install keyboard')
+import keyboard
 
 # Timing
-time_limit = 60  # seconds
-answer_cooldown = 30  # seconds
+time_limit = 12  # seconds
+answer_cooldown = 6  # seconds
 auto_show_answer = True
-auto_show_time = time_limit + answer_cooldown  # seconds
+auto_show_time = time_limit + 30  # seconds
 auto_rate_again = True
 auto_rate_time = 30
 # Keybindings
-close_app_keybind = 'ctrl+.'  # Don't use numpad '.'
-pause_app_keybind = 'ctrl+/'
-show_answer_keybind = '\''  # numbers are for numpad
-rate_again_keybind = '['
-rate_bad_keybind = ']'
-rate_good_keybind = '\\'
-rate_easy_keybind = ';'
-undo_answer_keybind = 'ctrl+\''
+#close_app_keybind = '-'  # Use + between combinations. Numbers are for num pad.
+#pause_app_keybind = '='
+#show_answer_keybind = '\''
+#rate_again_keybind = '['
+#rate_bad_keybind = ']'
+#rate_good_keybind = '\\'
+#rate_easy_keybind = ';'
+#undo_answer_keybind = '.'
+close_app_keybind = '-'  # Use + between combinations. Numbers are for num pad.
+pause_app_keybind = 'f20'
+show_answer_keybind = 'f14'
+rate_again_keybind = 'f15'
+rate_bad_keybind = 'f16'
+rate_good_keybind = 'f17'
+rate_easy_keybind = 'f18'
+undo_answer_keybind = 'f19'
 # Text Type
 text_color = "red"
-text_length_multiplier = 2  # 1 for english 2 for japanese
-text_length_multiplier2 = 1  # for showing the answer
+text_size = 1
 # Pathway
-question_pathway = 'result.fields.keyword.value'  # change this to the pathway of your deck's question and below one to the answer
-answer_pathway = 'result.fields.kanji.value'
+question_value = 'keyword'  # edit any card and place the field value you want as question/answer (cap sensitive)
+answer_value = 'kanji'
 # Other
 instant_answer = True  # True instantly show the answer, False fade the answer in over time
 
-############################################
-
 ANKI_URL = "http://localhost:8765"
 
-#  TODO
-#  don't destroy window, use same one to make faster
-#  stop use of ankiconnect, make into addon
 
+async def async_post(url, json):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=json) as response:
+            return await response.json()
 
-def getCurrentCard():
+async def getCurrentCard():
     payload = {
         "action": "guiCurrentCard",
         "version": 5
     }
-    r = requests.post(ANKI_URL, json=payload)
+    return await async_post(ANKI_URL, payload)
 
-    return r.json()
-
-
-def showAnswer():
+async def showAnswer():
     payload = {
         "action": "guiShowAnswer",
         "version": 5
     }
-    r = requests.post(ANKI_URL, json=payload)
+    return await async_post(ANKI_URL, payload)
 
-    return r.json()
-
-
-def undoAnswer():
+async def undoAnswer():
     payload = {
         "action": "guiUndo",
         "version": 5
     }
-    r = requests.post(ANKI_URL, json=payload)
+    return await async_post(ANKI_URL, payload)
 
-    return r.json()
-
-
-def rateCard(rate):
+async def rateCard(rate):
     payload = {
         "action": "guiAnswerCard",
         "version": 5,
@@ -80,21 +101,37 @@ def rateCard(rate):
             "ease": rate
         }
     }
-    r = requests.post(ANKI_URL, json=payload)
-
-    return r.json()
+    return await async_post(ANKI_URL, payload)
 
 
-# key hook to listen for ctrl+shift+.
+async def getNewText():
+    global answer_current_text, question_current_text
+    try:
+        card_data = await getCurrentCard()
+        answer_current_text = glom(card_data, 'result.fields.' + answer_value + '.value')
+        question_current_text = glom(card_data, 'result.fields.' + question_value + '.value')
+    except:
+        answer_current_text = "No Card"
+        question_current_text = "No Card"
+
+
+answer_current_text = "None"
+question_current_text = "None"
+
+
 class KeyWatcher:
     def __init__(self):
         self.running = True
         self.answer_showing = False
         self.paused = False
 
+    async def update_text_loop(self):
+        while self.running:
+            await getNewText()
+            await asyncio.sleep(1)
+
     def on_key_event(self, e):
-        if e.scan_code in range(2,
-                                12) or e.scan_code == 83:  # Remove "in range(2,12)" to allow normal numbers | Remove e.scan_code == 83 to allow numpad '.'
+        if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
             return False
         if keyboard.is_pressed(close_app_keybind):
             print("Stopping app!")
@@ -109,168 +146,155 @@ class KeyWatcher:
         if keyboard.is_pressed(rate_again_keybind):
             print("rate again")
             self.answer_showing = False
-            rateCard(1)
-            getNewText()
+            asyncio.run(rateCard(1))
         if keyboard.is_pressed(rate_bad_keybind):
             print("rate bad")
             self.answer_showing = False
-            rateCard(2)
-            getNewText()
+            asyncio.run(rateCard(2))
         if keyboard.is_pressed(rate_good_keybind):
             print("rate good")
             self.answer_showing = False
-            rateCard(3)
-            getNewText()
+            asyncio.run(rateCard(3))
         if keyboard.is_pressed(rate_easy_keybind):
             print("rate easy")
             self.answer_showing = False
-            rateCard(4)
-            getNewText()
+            asyncio.run(rateCard(4))
         if keyboard.is_pressed(undo_answer_keybind):
             print("undo answer")
             self.answer_showing = False
-            undoAnswer()
-            getNewText()
+            asyncio.run(undoAnswer())
         if keyboard.is_pressed(show_answer_keybind):
             print("show answer")
             self.answer_showing = True
-            showAnswer()
-            getNewText()
+            asyncio.run(showAnswer())
 
     def start_watching(self):
         keyboard.hook(self.on_key_event)
 
-
-# Create an instance of KeyWatcher
 key_watcher = KeyWatcher()
 
-# Create a thread to run the key watcher
-key_watcher_thread = threading.Thread(target=key_watcher.start_watching)
+class AnkiWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.Tool |
+            QtCore.Qt.WindowTransparentForInput
+        )
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setAttribute(QtCore.Qt.WA_ShowWithoutActivating)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.layout)
 
-# Start the thread
-key_watcher_thread.start()
+        self.text_widget = QtWidgets.QLabel("", self)
+        self.text_widget.setAlignment(QtCore.Qt.AlignCenter)
+        self.layout.addWidget(self.text_widget)
 
-current_text = None
-while not current_text:
-    try:
-        current_text = glom(getCurrentCard(), question_pathway)
-    except:
-        pass
+        self.question_text_widget = QtWidgets.QLabel("", self)
+        self.question_text_widget.setAlignment(QtCore.Qt.AlignCenter)
+        self.layout.addWidget(self.question_text_widget)
 
+        self.showFullScreen()
+        self.update_text()
 
-def getNewText():
-    global current_text
-    try:
-        if key_watcher.answer_showing:
-            current_text = glom(getCurrentCard(), answer_pathway)
-        else:
-            current_text = glom(getCurrentCard(), question_pathway)
-    except:
-        current_text = "no card"
+    def update_text(self):
+        global question_current_text, text_color, key_watcher, text_size
 
+        screen = self.screen()
+        screen_rect = screen.availableGeometry()
+        screen_width = screen_rect.width()
 
-def updateWindow():
-    global text_color
-    print(current_text)
+        font_size = int(min(screen_width * text_size / (len(question_current_text)), 450))
+        font = QtGui.QFont("BIZ UDGothic", font_size)
+        self.text_widget.setFont(font)
+        self.text_widget.setText(question_current_text)
+        self.text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
 
-    if key_watcher.answer_showing:
-        font_size = int(min(1920 / (len(current_text) * text_length_multiplier2), 450))
-        layout = [
-            [
-                sg.Text(
-                    glom(getCurrentCard(), question_pathway),
-                    size=len(current_text) * text_length_multiplier2,
-                    background_color="green",
-                    text_color=text_color,
-                    expand_x=True,
-                    expand_y=True,
-                    font=("BIZ UDGothic", font_size, "")
-                )
-            ],
-            [
-                sg.Text(
-                    current_text,
-                    size=len(current_text) * text_length_multiplier2,
-                    background_color="green",
-                    text_color=text_color,
-                    expand_x=True,
-                    expand_y=True,
-                    font=("BIZ UDGothic", font_size, "")
-                )
-            ]
-        ]
-    else:
-        font_size = int(min(1920 / (len(current_text) * text_length_multiplier), 450))
-        layout = [
-            [
-                sg.Text(
-                    current_text,
-                    size=len(current_text) * text_length_multiplier,
-                    background_color="green",
-                    text_color=text_color,
-                    expand_x=True,
-                    expand_y=True,
-                    font=("BIZ UDGothic", font_size, "")
-                )
-            ]
-        ]
+        font_size_question = int(min(screen_width * text_size / (len(answer_current_text)), 400))
+        font_question = QtGui.QFont("BIZ UDGothic", font_size_question)
+        self.question_text_widget.setFont(font_question)
+        self.question_text_widget.setText(answer_current_text)
+        self.question_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
+        self.question_text_widget.setVisible(key_watcher.answer_showing)
 
-    window = sg.Window(
-        "Window Title",
-        layout,
-        use_default_focus=False,
-        transparent_color="green",
-        keep_on_top=True,
-        no_titlebar=True,
-        background_color="green",
-        alpha_channel=0
-    )
-
-    window.FocusSet = False
-    return window
+    def set_opacity(self, opacity):
+        self.setWindowOpacity(opacity)
 
 
-def windowThing():
-    global current_text
-    last_text = current_text
-
-    window = updateWindow()
-    window.read(timeout=10)
-
+async def windowThing(window):
+    global question_current_text
     startTime = time.time()
+    current_text = question_current_text
+    current_step = key_watcher.answer_showing
 
-    while current_text == last_text and key_watcher.running:
+    while current_step == key_watcher.answer_showing and current_text == question_current_text and key_watcher.running:
+        await asyncio.sleep(0.1)
         now = time.time()
-        fadeAmount = min((now - startTime) / time_limit, 1)
-        getNewText()
+        fadeAmount = min((now - startTime) / time_limit, 1) ** 3
         if auto_show_answer and not key_watcher.answer_showing and now - startTime > auto_show_time:
+            print("Auto showing answer")
             key_watcher.answer_showing = True
-            showAnswer()
-            getNewText()
-        elif auto_rate_again and key_watcher.answer_showing and now - startTime > auto_rate_time:
+            await showAnswer()
+        elif auto_rate_again and key_watcher.answer_showing and current_step == True and now - startTime > auto_rate_time:
+            print("Auto rate again")
             key_watcher.answer_showing = False
-            rateCard(1)
-            getNewText()
+            await rateCard(1)
         if key_watcher.answer_showing and instant_answer:
-            window.set_alpha(1)
+            window.set_opacity(1)
         else:
-            window.set_alpha(fadeAmount)
+            window.set_opacity(fadeAmount)
         while key_watcher.paused:
-            window.set_alpha(0)
-            time.sleep(0.1)
-    window.close()  # close the window
+            await asyncio.sleep(0.1)
+            window.set_opacity(0)
+        QtCore.QCoreApplication.processEvents()
 
-    return current_text
+    window.set_opacity(0)
+
+    if current_text != question_current_text:
+        key_watcher.answer_showing = False
 
 
-# checks if ctrl + shift + . has been called
-while key_watcher.running:
-    print("cooldown!")
-    temp_time = answer_cooldown * 10
-    while temp_time > 0 and key_watcher.running and not key_watcher.answer_showing:
-        temp_time += -1
-        time.sleep(0.1)
-    if not key_watcher.answer_showing:
-        current_text = windowThing()
-    if key_watcher.answer_showing:
-        current_text = windowThing()
+async def main():
+    loop = asyncio.get_running_loop()
+    asyncio.create_task(key_watcher.update_text_loop())
+    loop.run_in_executor(None, key_watcher.start_watching)
+
+    app = QtWidgets.QApplication([])
+    window = AnkiWindow()
+
+    viewing_card = True
+    while True:
+        try:
+            card_data = await getCurrentCard()
+            question_current_text = glom(card_data, 'result.fields.' + question_value + '.value')
+            break
+        except KeyboardInterrupt:
+            break
+        except:
+            if viewing_card:
+                viewing_card = False
+                print("Start a studying session! \nSelected question value: " + question_value + " \nSelected answer value: " + answer_value)
+            if not key_watcher.running:
+                break
+            await asyncio.sleep(0.1)
+
+    while key_watcher.running:
+        print("cooldown!")
+        startTime = time.time()
+        now = time.time()
+        while not now - startTime > answer_cooldown and key_watcher.running and not key_watcher.answer_showing:
+            window.update_text()
+            await asyncio.sleep(0.1)
+            now = time.time()
+        if not key_watcher.answer_showing:
+            print(question_current_text)
+            await windowThing(window)
+        window.update_text()
+        if key_watcher.answer_showing:
+            await windowThing(window)
+
+    window.close()
+
+asyncio.run(main())
