733113502608beca07972b46d11b3f3adda68ee8
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-07-26 11:46:45 +0800

    added hooks, removed useless code, reformatting

--- __init__.py
+++ __init__.py
@@ -1,18 +1,14 @@
 # Author: ungodlyaura
 # Send bug reports or feature requests on the GitHub: https://github.com/ungodlyaura/AnkiAnnoyer
-print("starting1")
-from aqt import mw
+
 import os
-import time
 import sys
-sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 import keyboard
-# from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
-
-# import aqt.qt as qt
+import time
+from aqt import mw, gui_hooks
 from aqt.qt import QLabel, QVBoxLayout, QFont, QCoreApplication, QApplication, QWidget, Qt
-print("starting2")
-
 
 # Timing
 time_limit = 12  # seconds
@@ -21,15 +17,18 @@ auto_show_answer = True
 auto_show_time = time_limit + 30  # seconds
 auto_rate_again = True
 auto_rate_time = 30
+
 # Keybindings
-#close_app_keybind = '-'  # Use + between combinations. Numbers are for num pad.
-#pause_app_keybind = '='
-#show_answer_keybind = '\''
-#rate_again_keybind = '['
-#rate_bad_keybind = ']'
-#rate_good_keybind = '\\'
-#rate_easy_keybind = ';'
-#undo_answer_keybind = '.'
+'''
+close_app_keybind = '-'  # Use + between combinations. Numbers are for num pad.
+pause_app_keybind = '='
+show_answer_keybind = '\''
+rate_again_keybind = '['
+rate_bad_keybind = ']'
+rate_good_keybind = '\\'
+rate_easy_keybind = ';'
+undo_answer_keybind = '.'
+'''
 close_app_keybind = 'f21'  # Use + between combinations. Numbers are for num pad.
 pause_app_keybind = 'f20'
 show_answer_keybind = 'f14'
@@ -39,68 +38,68 @@ rate_good_keybind = 'f17'
 rate_easy_keybind = 'f18'
 undo_answer_keybind = 'f19'
 # Text Type
+opacity_scale = 3  # 1 = linear | higher means exponential | recommend around 3
 text_color = "red"
 text_size = 1
-# Pathway
-question_value = 'keyword'  # edit any card and place the field value you want as question/answer (cap sensitive)
-answer_value = 'kanji'
+font_style = "BIZ UDGothic"
 # Other
-instant_answer = True  # True instantly show the answer, False fade the answer in over time
+instant_answer = True  # True instantly show the answer | False fade the answer in
 
 
-def showAnswer():
+##################################################################################################
+
+def show_answer():
+    # noinspection PyProtectedMember
     mw.reviewer._showAnswer()
 
 
-def undoAnswer():
+def undo_answer():
     mw.undo()
 
 
-def rateCard(ease):
+def rate_card(ease):
+    if mw.reviewer.state == "question":
+        # noinspection PyProtectedMember
+        mw.reviewer._showAnswer()
     mw.reviewer.card.answer(ease)
 
 
-answer_showing = False
-paused = True
+paused = False
+cooldown = False
+startTime = time.time()
 
 
 def on_key_event(e):
-    global answer_showing, paused
-    if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
-        return False
-    if keyboard.is_pressed(rate_again_keybind):
-        print("rate again")
-        answer_showing = False
-        rateCard(1)
-    if keyboard.is_pressed(rate_bad_keybind):
-        print("rate bad")
-        answer_showing = False
-        rateCard(2)
-    if keyboard.is_pressed(rate_good_keybind):
-        print("rate good")
-        answer_showing = False
-        rateCard(3)
-    if keyboard.is_pressed(rate_easy_keybind):
-        print("rate easy")
-        answer_showing = False
-        rateCard(4)
-    if keyboard.is_pressed(undo_answer_keybind):
-        print("undo answer")
-        answer_showing = False
-        undoAnswer()
-    if keyboard.is_pressed(show_answer_keybind):
-        print("show answer")
-        answer_showing = True
-        showAnswer()
+    global paused
+    if not paused:
+        if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
+            return False
+        if keyboard.is_pressed(rate_again_keybind):
+            print("rate again")
+            rate_card(1)
+        if keyboard.is_pressed(rate_bad_keybind):
+            print("rate bad")
+            rate_card(2)
+        if keyboard.is_pressed(rate_good_keybind):
+            print("rate good")
+            rate_card(3)
+        if keyboard.is_pressed(rate_easy_keybind):
+            print("rate easy")
+            rate_card(4)
+        if keyboard.is_pressed(undo_answer_keybind):
+            print("undo answer")
+            undo_answer()
+        if keyboard.is_pressed(show_answer_keybind):
+            print("show answer")
+            show_answer()
 
 
+# Use keyboard's hook for capturing key's globally
 keyboard.hook(on_key_event)
 
-answer_current_text = "None"
-question_current_text = "None"
 
-
-class AnkiWindow(QWidget):
+# Use for creating an invisible window object that displays text
+class WindowObject(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(
@@ -127,86 +126,88 @@ class AnkiWindow(QWidget):
         self.update_text()
 
     def update_text(self):
-        global question_current_text, text_color, text_size, answer_showing
-
+        global text_color, text_size
+        answer_current_text = mw.reviewer.card.answer()
+        question_current_text = mw.reviewer.card.question()
         screen = self.screen()
         screen_rect = screen.availableGeometry()
         screen_width = screen_rect.width()
 
         font_size = int(min(screen_width * text_size / (len(question_current_text)), 450))
-        font = QFont("BIZ UDGothic", font_size)
+        font = QFont(font_style, font_size)
         self.text_widget.setFont(font)
         self.text_widget.setText(question_current_text)
         self.text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
 
         font_size_question = int(min(screen_width * text_size / (len(answer_current_text)), 400))
-        font_question = QFont("BIZ UDGothic", font_size_question)
+        font_question = QFont(font_style, font_size_question)
         self.question_text_widget.setFont(font_question)
         self.question_text_widget.setText(answer_current_text)
         self.question_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
-        self.question_text_widget.setVisible(answer_showing)
+        if mw.reviewer.state == "question":
+            self.question_text_widget.setVisible(False)
+        else:
+            self.question_text_widget.setVisible(True)
 
     def set_opacity(self, opacity):
         self.setWindowOpacity(opacity)
 
 
-def windowThing(window):
-    global question_current_text, answer_showing
+def window_loop(window):
+    global cooldown, startTime
     startTime = time.time()
-    current_text = question_current_text
-    current_step = answer_showing
-
-    while current_step == answer_showing and current_text == question_current_text and not paused:
-        # qWait(100)
+    while not cooldown:
         now = time.time()
-        fadeAmount = min((now - startTime) / time_limit, 1) ** 3
-        if auto_show_answer and not answer_showing and now - startTime > auto_show_time:
-            print("Auto showing answer")
-            answer_showing = True
-            showAnswer()
-        elif auto_rate_again and answer_showing and current_step == True and now - startTime > auto_rate_time:
-            print("Auto rate again")
-            answer_showing = False
-            rateCard(1)
-        if answer_showing and instant_answer:
-            window.set_opacity(1)
-        else:
-            window.set_opacity(fadeAmount)
+        window.set_opacity(min((now - startTime) / time_limit, 1) ** opacity_scale)
         QCoreApplication.processEvents()
-
-    window.set_opacity(0)
-
-    if current_text != question_current_text:
-        answer_showing = False
+        if mw.reviewer.state == "question" and auto_show_answer and now - startTime > auto_show_time:
+            print("Auto showing answer")
+            show_answer()
+        elif mw.reviewer.state == "answer" and auto_rate_again and now - startTime > auto_rate_time:
+            print("Auto rate again")
+            rate_card(1)
+        # wait
+        pass
 
 
 def main():
-    global question_current_text
-
+    global cooldown
+    # Make the window object (that displays text on screen)
     app = QApplication([])
-    window = AnkiWindow()
+    window = WindowObject()
 
     while True:
-        print("cooldown!")
-        while paused:
+        # Pause the script for the cooldown time
+        cooldown_start_time = time.time()
+        while mw.reviewer.state == "question" and not time.time() - cooldown_start_time > answer_cooldown:
             QCoreApplication.processEvents()
             pass
-            # qWait(100)
-        startTime = time.time()
-        now = time.time()
-        while not now - startTime > answer_cooldown and not answer_showing:
-            QCoreApplication.processEvents()
-            print(question_current_text)
-            window.update_text()
-            # qWait(100)
-            now = time.time()
-        if not answer_showing:
-            windowThing(window)
-        window.update_text()
-        if answer_showing:
-            windowThing(window)
+            # wait
+        cooldown = False
+
+        # If window_loop ending, it means new card.
+        window_loop(window)
+        window.set_opacity(0)
 
     window.close()
 
 
-main()
+def show_question():
+    global startTime, cooldown
+    # show_question means new card, so start a cooldown
+    cooldown = True
+
+
+def show_answer():
+    global startTime, instant_answer, time_limit
+
+    if instant_answer:
+        startTime = startTime - time_limit
+    else:
+        startTime = time.time()
+
+
+gui_hooks.reviewer_did_show_question.append(show_question)
+gui_hooks.reviewer_did_show_answer.append(show_answer)
+
+#main()
