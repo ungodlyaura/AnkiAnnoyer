4b4b01a2cce064a23dfea343ed2e5fc7fb456e34
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-07-26 20:53:40 +0800

    somewhat working

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 import keyboard
 import time
 from aqt import mw, gui_hooks
-from aqt.qt import QLabel, QVBoxLayout, QFont, QCoreApplication, QWidget, Qt, pyqtSignal, QMetaObject, Q_ARG
+from aqt.qt import QLabel, QVBoxLayout, QFont, QCoreApplication, QWidget, Qt, pyqtSignal
 
 # Timing
 time_limit = 12  # seconds
@@ -41,13 +41,18 @@ undo_answer_keybind = 'f19'
 # Text Type
 opacity_scale = 3  # 1 = linear | higher means exponential | recommend around 3
 text_color = "red"
-text_size = 1
+text_size = 5
+text_size2 = 1
 font_style = "BIZ UDGothic"
 # Other
 instant_answer = True  # True instantly show the answer | False fade the answer in
-
+paused = False  # Is the addon paused upon loading?
 
 ##################################################################################################
+
+cooldown = False
+startTime = time.time()
+
 
 def show_answer():
     # noinspection PyProtectedMember
@@ -67,40 +72,6 @@ def rate_card(ease):
     mw.reviewer._answerCard(ease)
 
 
-paused = False
-cooldown = False
-startTime = time.time()
-
-
-def on_key_event(e):
-    global paused
-    if not paused:
-        if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
-            return False
-        if keyboard.is_pressed(rate_again_keybind):
-            print("rate again")
-            rate_card(1)
-        if keyboard.is_pressed(rate_bad_keybind):
-            print("rate bad")
-            rate_card(2)
-        if keyboard.is_pressed(rate_good_keybind):
-            print("rate good")
-            rate_card(3)
-        if keyboard.is_pressed(rate_easy_keybind):
-            print("rate easy")
-            rate_card(4)
-        if keyboard.is_pressed(undo_answer_keybind):
-            print("undo answer")
-            undo_answer()
-        if keyboard.is_pressed(show_answer_keybind):
-            print("show answer")
-            show_answer()
-
-
-# Use keyboard's hook for capturing key's globally
-keyboard.hook(on_key_event)
-
-
 def process_events():
     QCoreApplication.processEvents()
 
@@ -112,6 +83,7 @@ class WindowObject(QWidget):
     process_events_signal = pyqtSignal()
     show_answer_signal = pyqtSignal()
     rate_signal = pyqtSignal(int)
+    undo_signal = pyqtSignal()
 
     def __init__(self):
         super().__init__()
@@ -127,13 +99,13 @@ class WindowObject(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.text_widget = QLabel("", self)
-        self.text_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.layout.addWidget(self.text_widget)
-
         self.question_text_widget = QLabel("", self)
         self.question_text_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.question_text_widget)
+
+        self.answer_text_widget = QLabel("", self)
+        self.answer_text_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.answer_text_widget)
 
         self.showFullScreen()
 
@@ -142,36 +114,39 @@ class WindowObject(QWidget):
         self.process_events_signal.connect(process_events)
         self.show_answer_signal.connect(show_answer)
         self.rate_signal.connect(rate_card)
+        self.undo_signal.connect(undo_answer)
 
     def update_text(self):
         print("updating text")
         global text_color, text_size
-        answer_current_text = mw.reviewer.card.answer()
-        if not answer_current_text:
-            answer_current_text = "None"
-        question_current_text = mw.reviewer.card.question()
-        if not question_current_text:
-            answer_current_text = "None"
-        print(answer_current_text)
+        answer_current_text = "None"
+        question_current_text = "None"
+
+        if mw.reviewer.card and mw.reviewer.card.answer():
+            answer_current_text = mw.reviewer.card.answer()
+            question_current_text = mw.reviewer.card.question()
+
         screen = self.screen()
         screen_rect = screen.availableGeometry()
         screen_width = screen_rect.width()
 
         font_size = int(min(screen_width * text_size / (len(question_current_text)), 450))
         font = QFont(font_style, font_size)
-        self.text_widget.setFont(font)
-        self.text_widget.setText(question_current_text)
-        self.text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
-
-        font_size_question = int(min(screen_width * text_size / (len(answer_current_text)), 400))
-        font_question = QFont(font_style, font_size_question)
-        self.question_text_widget.setFont(font_question)
-        self.question_text_widget.setText(answer_current_text)
+        self.question_text_widget.setFont(font)
+        self.question_text_widget.setText(question_current_text)
         self.question_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
+
+        font_size_question = int(min(screen_width * text_size2 / (len(answer_current_text)), 400))
+        font_question = QFont(font_style, font_size_question)
+        self.answer_text_widget.setFont(font_question)
+        self.answer_text_widget.setText(answer_current_text)
+        self.answer_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
         if mw.reviewer.state == "question":
-            self.question_text_widget.setVisible(False)
-        else:
             self.question_text_widget.setVisible(True)
+            self.answer_text_widget.setVisible(False)
+        else:
+            self.question_text_widget.setVisible(False)
+            self.answer_text_widget.setVisible(True)
 
     def set_opacity(self, opacity):
         self.setWindowOpacity(opacity)
@@ -187,17 +162,19 @@ class BackgroundTask(threading.Thread):
         global startTime
         startTime = time.time()
         print("new card")
+        self.window.update_text_signal.emit()
         while not cooldown and not paused:
             now = time.time()
             opacity = min((now - startTime) / time_limit, 1) ** opacity_scale
             # Emit signal to update opacity
             self.window.set_opacity_signal.emit(opacity)
-            self.window.update_text_signal.emit()
-            print(opacity)
             self.window.process_events_signal.emit()
+
             if mw.reviewer.state == "question" and auto_show_answer and now - startTime > auto_show_time:
                 print("Auto showing answer")
                 self.window.show_answer_signal.emit()
+                startTime = time.time()
+                self.window.update_text_signal.emit()
             elif mw.reviewer.state == "answer" and auto_rate_again and now - startTime > auto_rate_time:
                 print("Auto rate again")
                 self.window.rate_signal.emit(1)
@@ -206,7 +183,6 @@ class BackgroundTask(threading.Thread):
 
     def run(self):
         global cooldown, paused
-        # Make the window object (that displays text on screen)
 
         while not paused:
             # Pause the script for the cooldown time
@@ -239,15 +215,41 @@ def on_show_answer(card):
         startTime = time.time()
 
 
+class Main:
+    def on_key_event(self, e):
+        global paused
+        if not paused:
+            if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
+                return False
+            if keyboard.is_pressed(rate_again_keybind):
+                print("rate again")
+                self.window.rate_signal.emit(1)
+            if keyboard.is_pressed(rate_bad_keybind):
+                print("rate bad")
+                self.window.rate_signal.emit(2)
+            if keyboard.is_pressed(rate_good_keybind):
+                print("rate good")
+                self.window.rate_signal.emit(3)
+            if keyboard.is_pressed(rate_easy_keybind):
+                print("rate easy")
+                self.window.rate_signal.emit(4)
+            if keyboard.is_pressed(undo_answer_keybind):
+                print("undo answer")
+                self.window.undo_signal.emit()
+            if keyboard.is_pressed(show_answer_keybind):
+                print("show answer")
+                self.window.show_answer_signal.emit()
+
+    def __init__(self):
+        print("running main")
+        # Make the window object (that displays text on screen)
+        self.window = WindowObject()
+        # Use keyboard's hook for capturing key's globally
+        keyboard.hook(self.on_key_event)
+        background_task = BackgroundTask(self.window)
+        background_task.start()
+
+
 gui_hooks.reviewer_did_show_question.append(on_show_question)
 gui_hooks.reviewer_did_show_answer.append(on_show_answer)
-
-
-def main():
-    print("running main")
-    window = WindowObject()
-    background_task = BackgroundTask(window)
-    background_task.start()
-
-
-main()
+Main()
