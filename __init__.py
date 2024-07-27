7077525bbffdd2ead20bf1c8a77215be727eebdc
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-07-27 09:30:01 +0800

    added config.json

--- __init__.py
+++ __init__.py
@@ -11,45 +11,9 @@ import time
 from aqt import mw, gui_hooks
 from aqt.qt import QLabel, QVBoxLayout, QFont, QCoreApplication, QWidget, Qt, pyqtSignal
 
-# Timing
-time_limit = 12  # seconds
-answer_cooldown = 6  # seconds
-auto_show_answer = True
-auto_show_time = time_limit + 30  # seconds
-auto_rate_again = True
-auto_rate_time = 30
+config = mw.addonManager.getConfig(__name__)
 
-# Keybindings
-'''
-close_app_keybind = '-'  # Use + between combinations. Numbers are for num pad.
-pause_app_keybind = '='
-show_answer_keybind = '\''
-rate_again_keybind = '['
-rate_bad_keybind = ']'
-rate_good_keybind = '\\'
-rate_easy_keybind = ';'
-undo_answer_keybind = '.'
-'''
-close_app_keybind = 'f21'  # Use + between combinations. Numbers are for num pad.
-pause_app_keybind = 'f20'
-show_answer_keybind = 'f14'
-rate_again_keybind = 'f15'
-rate_bad_keybind = 'f16'
-rate_good_keybind = 'f17'
-rate_easy_keybind = 'f18'
-undo_answer_keybind = 'f19'
-# Text Type
-opacity_scale = 3  # 1 = linear | higher means exponential | recommend around 3
-text_color = "red"
-text_size = 5
-text_size2 = 1
-font_style = "BIZ UDGothic"
-# Other
-instant_answer = True  # True instantly show the answer | False fade the answer in
 paused = False  # Is the addon paused upon loading?
-
-##################################################################################################
-
 cooldown = False
 startTime = time.time()
 
@@ -118,12 +82,11 @@ class WindowObject(QWidget):
 
     def update_text(self):
         print("updating text")
-        global text_color, text_size
         answer_current_text = "None"
         question_current_text = "None"
 
         if mw.reviewer.card and mw.reviewer.card.answer():
-            print (mw.reviewer.card.question())
+            print(mw.reviewer.card.question())
             answer_current_text = mw.reviewer.card.answer()
             question_current_text = mw.reviewer.card.question()
 
@@ -131,17 +94,17 @@ class WindowObject(QWidget):
         screen_rect = screen.availableGeometry()
         screen_width = screen_rect.width()
 
-        font_size = int(min(screen_width * text_size / (len(question_current_text)), 450))
-        font = QFont(font_style, font_size)
+        font_size = int(min(screen_width * config['text_size'] / (len(question_current_text)), 450))
+        font = QFont(config['font_style'], font_size)
         self.question_text_widget.setFont(font)
         self.question_text_widget.setText(question_current_text)
-        self.question_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
+        self.question_text_widget.setStyleSheet(f"color: {config['text_color']}; background: transparent;")
 
-        font_size_question = int(min(screen_width * text_size2 / (len(answer_current_text)), 400))
-        font_question = QFont(font_style, font_size_question)
+        font_size_question = int(min(screen_width * config['text_size2'] / (len(answer_current_text)), 400))
+        font_question = QFont(config['font_style'], font_size_question)
         self.answer_text_widget.setFont(font_question)
         self.answer_text_widget.setText(answer_current_text)
-        self.answer_text_widget.setStyleSheet(f"color: {text_color}; background: transparent;")
+        self.answer_text_widget.setStyleSheet(f"color: {config['text_color']}; background: transparent;")
         if mw.reviewer.state == "question":
             self.question_text_widget.setVisible(True)
             self.answer_text_widget.setVisible(False)
@@ -163,20 +126,19 @@ class BackgroundTask(threading.Thread):
         global startTime
         startTime = time.time()
         print("new card")
-        self.window.update_text_signal.emit()
         while not cooldown and not paused:
             now = time.time()
-            opacity = min((now - startTime) / time_limit, 1) ** opacity_scale
+            opacity = min((now - startTime) / config['time_limit'], 1) ** config['opacity_scale']
             # Emit signal to update opacity
             self.window.set_opacity_signal.emit(opacity)
+            self.window.update_text_signal.emit()
             self.window.process_events_signal.emit()
 
-            if mw.reviewer.state == "question" and auto_show_answer and now - startTime > auto_show_time:
+            if mw.reviewer.state == "question" and config['auto_show_answer'] and now - startTime > config['auto_show_time']:
                 print("Auto showing answer")
                 self.window.show_answer_signal.emit()
                 startTime = time.time()
-                self.window.update_text_signal.emit()
-            elif mw.reviewer.state == "answer" and auto_rate_again and now - startTime > auto_rate_time:
+            elif mw.reviewer.state == "answer" and config['auto_rate_again'] and now - startTime > config['auto_rate_time']:
                 print("Auto rate again")
                 self.window.rate_signal.emit(1)
 
@@ -189,7 +151,7 @@ class BackgroundTask(threading.Thread):
             # Pause the script for the cooldown time
             print("cooldown")
             cooldown_start_time = time.time()
-            while mw.reviewer.state != "answer" and not time.time() - cooldown_start_time > answer_cooldown:
+            while mw.reviewer.state != "answer" and not time.time() - cooldown_start_time > config['answer_cooldown']:
                 self.window.process_events_signal.emit()
                 time.sleep(0.1)
             cooldown = False
@@ -208,12 +170,24 @@ def on_show_question(card):
 
 
 def on_show_answer(card):
-    global startTime, instant_answer, time_limit
+    global startTime
     print("answer shown")
-    if instant_answer:
-        startTime = startTime - time_limit
+    if config['instant_answer']:
+        startTime = startTime - config['time_limit']
     else:
         startTime = time.time()
+
+
+def on_pause_key_event(e):
+    global paused
+    if keyboard.is_pressed(config['pause_app_keybind']):
+        if paused:
+            paused = False
+        else:
+            paused = True
+
+
+keyboard.hook(on_pause_key_event)
 
 
 class Main:
@@ -222,22 +196,22 @@ class Main:
         if not paused:
             if e.scan_code in range(2, 12):  # Remove "in range(2,12)" to allow normal numbers
                 return False
-            if keyboard.is_pressed(rate_again_keybind):
+            if keyboard.is_pressed(config['rate_again_keybind']):
                 print("rate again")
                 self.window.rate_signal.emit(1)
-            if keyboard.is_pressed(rate_bad_keybind):
+            if keyboard.is_pressed(config['rate_bad_keybind']):
                 print("rate bad")
                 self.window.rate_signal.emit(2)
-            if keyboard.is_pressed(rate_good_keybind):
+            if keyboard.is_pressed(config['rate_good_keybind']):
                 print("rate good")
                 self.window.rate_signal.emit(3)
-            if keyboard.is_pressed(rate_easy_keybind):
+            if keyboard.is_pressed(config['rate_easy_keybind']):
                 print("rate easy")
                 self.window.rate_signal.emit(4)
-            if keyboard.is_pressed(undo_answer_keybind):
+            if keyboard.is_pressed(config['undo_answer_keybind']):
                 print("undo answer")
                 self.window.undo_signal.emit()
-            if keyboard.is_pressed(show_answer_keybind):
+            if keyboard.is_pressed(config['show_answer_keybind']):
                 print("show answer")
                 self.window.show_answer_signal.emit()
 
